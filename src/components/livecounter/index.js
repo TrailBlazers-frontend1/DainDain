dfdeb2c63bc1d20f992992db6fbe010006fe2e94
Thurein Win <tblazersfrontend1@gmail.com>
2022-09-14 21:18:35 +0630

    heroku test

--- src/components/livecounter/index.js
+++ src/components/livecounter/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./styles.css"
-import moment from 'moment'
+// import moment from 'moment'
 import { useSelector } from 'react-redux'
 import { isMorningOrEvening, threeDCountDown } from '../../redux/countdown'
 
